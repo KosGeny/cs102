44b914465919284875759d528cf9f4ed71370a26
KosGeny <k_genne@mail.ru>
2024-04-26 23:10:53 +0300

    Добавлены и обновлены тесты для функций encrypt_caesar() и decrypt_caesar()

--- tests/lab2/test_caesar.py
+++ tests/lab2/test_caesar.py
@@ -7,7 +7,8 @@ class CaesarTestCase(unittest.TestCase):
     def test_encrypt_caesar(self):
         self.assertEqual(encrypt_caesar('hello'), 'khoor')
         self.assertEqual(encrypt_caesar('ITMO'), 'LWPR')
-        self.assertEqual(encrypt_caesar('xyz'), 'abc')
+        self.assertEqual(encrypt_caesar('ITMO', 4), 'MXQS')
+        self.assertEqual(encrypt_caesar('stu', 5), 'xyz')
         self.assertEqual(encrypt_caesar('xyz2-3'), 'abc2-3')
         self.assertEqual(encrypt_caesar('52'), '52')
         self.assertEqual(encrypt_caesar(''), '')
@@ -15,7 +16,8 @@ class CaesarTestCase(unittest.TestCase):
     def test_decrypt_caesar(self):
         self.assertEqual(decrypt_caesar('khoor'), 'hello')
         self.assertEqual(decrypt_caesar('LWPR'), 'ITMO')
-        self.assertEqual(decrypt_caesar('abc'), 'xyz')
+        self.assertEqual(decrypt_caesar('MXQS', 4), 'ITMO')
+        self.assertEqual(decrypt_caesar('xyz', 5), 'stu')
         self.assertEqual(decrypt_caesar('abc2-3'), 'xyz2-3')
         self.assertEqual(decrypt_caesar('52'), '52')
         self.assertEqual(decrypt_caesar(''), '')
