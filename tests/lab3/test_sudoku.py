16f9dde70325a293bd53a78072736bd233b0a6f1
KosGeny <k_genne@mail.ru>
2024-04-27 14:58:52 +0300

    Добавлены тесты для функции get_row()

--- tests/lab3/test_sudoku.py
+++ tests/lab3/test_sudoku.py
@@ -14,5 +14,24 @@ class SudokuTestCase(unittest.TestCase):
         actual_groups = sudoku.group(values, 3)
         self.assertEqual(needed_groups, actual_groups)
 
+    def test_get_row(self):
+        puzzle = [['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']]
+        pos = (0, 0)
+        needed_row = ['1', '2', '.']
+        actual_row = sudoku.get_row(puzzle, pos)
+        self.assertEqual(needed_row, actual_row)
+
+        puzzle = [['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']]
+        pos = (1, 0)
+        needed_row = ['4', '.', '6']
+        actual_row = sudoku.get_row(puzzle, pos)
+        self.assertEqual(needed_row, actual_row)
+
+        puzzle = [['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']]
+        pos = (2, 0)
+        needed_row = ['.', '8', '9']
+        actual_row = sudoku.get_row(puzzle, pos)
+        self.assertEqual(needed_row, actual_row)
+
 if __name__ == '__main__':
     unittest.main()
