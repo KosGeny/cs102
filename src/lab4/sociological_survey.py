6fa47098c8055a39985894055d232289bab6814a
KosGeny <k_genne@mail.ru>
2024-04-29 00:01:11 +0300

    Реализован класс AgeGroupDivider, заменено название класса Age_Group -> AgeGroup

--- src/lab4/sociological_survey.py
+++ src/lab4/sociological_survey.py
@@ -1,4 +1,4 @@
-class Age_Group:
+class AgeGroup:
     def __init__(self, start, end):
         self.start = start
         self.end = end
@@ -22,6 +22,29 @@ class Respondent:
         return f"{self.name} ({self.age})"
 
 
+class AgeGroupDivider:
+    def __init__(self, age_groups):
+        self.age_groups = age_groups
+        self.grouped_respondents = {group: [] for group in self.age_groups}
+
+    def add_respondent(self, respondent):
+        for group in self.age_groups:
+            if group.contains(respondent.age):
+                self.grouped_respondents[group].append(respondent)
+                break
+
+    def display_groups(self):
+        for group in reversed(self.age_groups):
+            if self.grouped_respondents[group]:
+                print(f"{group.str()}: ", end="")
+                print(
+                    ", ".join(
+                        respondent.str()
+                        for respondent in sorted(self.grouped_respondents[group], key=lambda x: (-x.age, x.name))
+                    )
+                )
+
+
 def parse_age_groups(args):
     age_groups = [-1] + [int(arg) for arg in args] + [123]
-    return [Age_Group(age_groups[i] + 1, age_groups[i + 1]) for i in range(len(age_groups) - 1)]
+    return [AgeGroup(age_groups[i] + 1, age_groups[i + 1]) for i in range(len(age_groups) - 1)]
