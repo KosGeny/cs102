21b171caab9cec8140028bdc49f58cadb19436c9
KosGeny <k_genne@mail.ru>
2024-04-28 23:53:20 +0300

    Реализован класс Age_Group

---
+++ src/lab4/sociological_survey.py
@@ -0,0 +1,13 @@
+class Age_Group:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def contains(self, age):
+        return self.start < age <= self.end
+
+    def str(self):
+        if self.start == 101:
+            return "101+"
+
+        return f"{self.start}-{self.end}"
