40d951085f8cf2d873fb6ef14369755086ca79a7
KosGeny <k_genne@mail.ru>
2024-04-28 23:47:17 +0300

    Реализована функция recommend_film()

--- src/lab4/film_recomendation.py
+++ src/lab4/film_recomendation.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 PATH_TO_INPUT_FILMS = "input_1.txt"
 PATH_TO_INPUT_USERS = "input_2.txt"
 
@@ -46,7 +49,34 @@ class Film_DB(dict):
         return self.films
 
 
+def recommend_film(films, persons, current_user):
+    film_views_count = {}
+
+    user_films = set(current_user.films)
+
+    for person in persons:
+        person_films = set(person.films)
+        if 2 * len(person_films & user_films) >= len(user_films):
+            for film_id in person_films:
+                if film_id not in user_films:
+                    if film_id in film_views_count:
+                        film_views_count[film_id] += 1
+                    else:
+                        film_views_count[film_id] = 1
+
+    if not film_views_count:
+        return "No recommendations found."
+
+    max_views = max(film_views_count.values())
+    top_movies = [movie_id for movie_id, count in film_views_count.items() if count == max_views]
+    recommended_movie_id = top_movies[randint(0, len(top_movies) - 1)]
+    return films[recommended_movie_id].title
+
+
 film_db = Film_DB(PATH_TO_INPUT_FILMS)
 user_db = User_DB(PATH_TO_INPUT_USERS)
 
 current_user = User([int(film_id) for film_id in input().split(",")])
+
+recommended_movie = recommend_film(film_db.get_all(), user_db.get_all(), current_user)
+print(recommended_movie)
