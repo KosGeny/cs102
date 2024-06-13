f179dc91c91c951b0050c81fbfaf06813b24ef50
KosGeny <k_genne@mail.ru>
2024-06-13 12:05:40 +0300

    Изменил путь к файлам users.txt и films.txt и вновь добавил их в папку tests

--- src/lab4/film_recomendation.py
+++ src/lab4/film_recomendation.py
@@ -4,11 +4,11 @@ from random import randint
 
 
 file_dir = os.path.dirname(os.path.realpath("__file__"))
-PATH_TO_INPUT_FILMS = os.path.join(file_dir, "../../src/lab4/films.txt")
+PATH_TO_INPUT_FILMS = os.path.join(file_dir, "films.txt")
 PATH_TO_INPUT_FILMS = os.path.abspath(os.path.realpath(PATH_TO_INPUT_FILMS))
 
 file_dir = os.path.dirname(os.path.realpath("__file__"))
-PATH_TO_INPUT_USERS = os.path.join(file_dir, "../../src/lab4/users.txt")
+PATH_TO_INPUT_USERS = os.path.join(file_dir, "users.txt")
 PATH_TO_INPUT_USERS = os.path.abspath(os.path.realpath(PATH_TO_INPUT_USERS))
 
 
